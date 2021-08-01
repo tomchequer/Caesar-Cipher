c0e2c0574046edcef02d6fb7ec5ccd3e81ebd9a3
antonio <antoniochequer@gmail.com>
2021-08-01 15:39:21 -0300

    finished most of the programm.

--- encryption.py
+++ encryption.py
@@ -1,2 +1,34 @@
 #This should be a programm that will be able to encrypt
 #and decrypt data stored in Caesar Cipher
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+
+def encrypt(text_par, shift_par):
+    ciphertext = ''
+    for i in text:
+        position = alphabet.index(i)
+        new_position = position + shift_par
+        new_letter = alphabet[new_position]
+        ciphertext += new_letter
+    print(f"The encoded message is: '{ciphertext}'")
+
+def decrypt (text_par, shift_par):
+    decryptedtext = ''
+    for i in text:
+        position = alphabet.index(i)
+        oldposition = position - shift_par
+        old_letter = alphabet[oldposition]
+        decryptedtext += old_letter
+    print(f'The decrypted message is "{decryptedtext}"".')
+
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
+    
